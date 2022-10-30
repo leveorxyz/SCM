87139a6d382dfba21e5d2d0afa77c9c3249a8a9a
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-31 03:57:52 +0600

    Update(UI): container theme

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,19 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
+import { Flex, FlexProps, useColorModeValue } from "@chakra-ui/react";
 
-export const Container = (props: FlexProps) => (
-  <Flex
-    direction="column"
-    alignItems="center"
-    justifyContent="flex-start"
-    bg="gray.50"
-    color="black"
-    _dark={{
-      bg: 'gray.900',
-      color: 'white',
-    }}
-    transition="all 0.15s ease-out"
-    {...props}
-  />
-)
+export const Container = (props: FlexProps) => {
+  const bgColor = useColorModeValue("gray.100", "gray.900");
+  const color = useColorModeValue("gray.900", "gray.100");
+
+  return (
+    <Flex
+      id="container"
+      direction="column"
+      alignItems="center"
+      justifyContent="flex-start"
+      bgColor={bgColor}
+      color={color}
+      transition="all 0.15s ease-out"
+      {...props}
+    />
+  );
+};
